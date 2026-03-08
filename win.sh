0d1c82b7e20b8c41e3a461838917b95cb13136a7
haduydong0797-sys <haduydong0797@gmail.com>
2026-03-09 01:46:50 +0700

    Update win.sh

--- win.sh
+++ win.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-IMG_URL="https://dumgzwin.atl1.cdn.digitaloceanspaces.com/winDO66auto.gz"
+IMG_URL="https://dumgzwin.atl1.cdn.digitaloceanspaces.com/winDO77.gz"
 
 echo "================================="
 echo "     Windows VPS Installer"
